078caa12974d248caf7eeae94fdc649e48c22c4b
Daniel Karanja <sirkaranja12@gmail.com>
2023-03-19 19:20:40 +0300

    updated

--- grading.js
+++ grading.js
@@ -10,16 +10,15 @@ function studentGrade(marks, grade) {
 
 
   //introduced if else statement to check the conditions of marks from user input
-if (marks >= 80 && marks <= 100) {
+if (marks >= 80 && marks <= 100) {     //checks the conditions of marks if its between 80 and 100
    grade = "A";
-} else if (marks >= 60 && marks <= 79) {
+} else if (marks >= 60 && marks <= 79) {   //checks the conditions of marks if its between 60 and 79
    grade = "B";
-} else if (marks >= 50 && marks <= 59) {
+} else if (marks >= 50 && marks <= 59) {     //checks the conditions of marks if its between 50 and 59
 grade = "C";
-} else if (marks >= 40 && marks <= 49) {
+} else if (marks >= 40 && marks <= 49) {    //checks the conditions of marks if its between 40 and 49
   grade = "D";
-} else if (marks >= 0 && marks < 40) {
-  grade = "E";
+} else if (marks >= 0 && marks < 40) {      //checks the conditions of marks if its between 0 and 40
 } else {
 
  //checks if number input is not between 0 and 100 it should output an error message of invalid input 
